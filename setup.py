210fdac8d3a1423dea9ebbdf420ee263b75d390b
Kyle Dunovan <dunovank@gmail.com>
2018-10-25 14:55:52 -0400

    update Lab3 nb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_data = {'ADMCode':['notebooks/*.ipynb', 'notebooks/images/*.png', 'noteb
 
 major = 0
 minor = 4
-patch = 0
+patch = 2
 __version__ = '.'.join([str(v) for v in [major, minor, patch]])
 
 setup(
